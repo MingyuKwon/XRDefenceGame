01d2eda855b925410e68d226388094fcdbd04add
Kwon Mingyu <kmgmg230601@gmail.com>
2024-06-22 16:48:20 +0900

    일단 처음 게임 시작하면 플레이어 앞에 게임 맵이 나오도록 했습니다

---
+++ Source/XRDefenceGame/Public/GameMode/GamePlayMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "GamePlayMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class XRDEFENCEGAME_API AGamePlayMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
