9ebb654c1b58d99e754775645e4fcaa0922ae810
Kwon Mingyu <kmgmg230601@gmail.com>
2024-07-16 23:01:53 +0900

    위젯 텍스트들 바꾸는 함수 실 적용 되는거 확인했습니다

--- Source/XRDefenceGame/Private/UI/GamePlayHandUI.cpp
+++ Source/XRDefenceGame/Private/UI/GamePlayHandUI.cpp
@@ -1,5 +1,59 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "UI/GamePlayHandUI.h"
+#include "Components/TextBlock.h"
 
+void UGamePlayHandUI::SetTimeText(float LeftSecond)
+{
+    int32 Minutes = FMath::FloorToInt(LeftSecond / 60.0f);
+    int32 Seconds = FMath::FloorToInt(FMath::Fmod(LeftSecond, 60.0f));
+    FString TimeString = FString::Printf(TEXT("%02d:%02d"), Minutes, Seconds);
+    FText TimeTextValue = FText::FromString(TimeString);
+    if (TimeText)
+    {
+        TimeText->SetText(TimeTextValue);
+    }
+}
+
+void UGamePlayHandUI::SetGoldText(float GoldAmount)
+{
+    FText GoldTextValue = FText::AsNumber(GoldAmount);
+    if (GoldText)
+    {
+        GoldText->SetText(GoldTextValue);
+    }
+}
+
+void UGamePlayHandUI::SetBlueHeartText(float HealthAmount)
+{
+    FText BlueHeartTextValue = FText::AsNumber(HealthAmount);
+    if (BlueHeartText)
+    {
+        BlueHeartText->SetText(BlueHeartTextValue);
+    }
+}
+
+void UGamePlayHandUI::SetPurpleHeartText(float HealthAmount)
+{
+    FText PurpleHeartTextValue = FText::AsNumber(HealthAmount);
+    if (PurpleHeartText)
+    {
+        PurpleHeartText->SetText(PurpleHeartTextValue);
+    }
+}
+
+void UGamePlayHandUI::SetOrangeHeartText(float HealthAmount)
+{
+    FText OrangeHeartTextValue = FText::AsNumber(HealthAmount);
+    if (OrangeHeartText)
+    {
+        OrangeHeartText->SetText(OrangeHeartTextValue);
+    }
+}
+
+void UGamePlayHandUI::SetHealthText(float HealthAmount)
+{
+    FText HealthTextValue = FText::AsNumber(HealthAmount);
+    if (HealthText)
+    {
+        HealthText->SetText(HealthTextValue);
+    }
+}
