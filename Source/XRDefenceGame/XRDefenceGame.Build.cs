5277c9f74704748f2c33462dc395bcf1fb347ac5
Kwon Mingyu <kmgmg230601@gmail.com>
2024-07-28 17:39:00 +0900

    멀티 플레이 서브 시스템 추가했습니다

--- Source/XRDefenceGame/XRDefenceGame.Build.cs
+++ Source/XRDefenceGame/XRDefenceGame.Build.cs
@@ -8,7 +8,7 @@ public class XRDefenceGame : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" , "Niagara", "MotionWarping" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" , "Niagara", "MotionWarping", "OnlineSubsystemSteam", "OnlineSubsystem", "MultiplayerSession" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
