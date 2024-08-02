30efe2ece35de66548eda4d94ccd9f939f05082a
Kwon Mingyu <kmgmg230601@gmail.com>
2024-08-02 13:52:39 +0900

    밸패 1

--- Source/XRDefenceGame/Private/Character/Deffence/MyXR_CharacterDeffenceBuff.cpp
+++ Source/XRDefenceGame/Private/Character/Deffence/MyXR_CharacterDeffenceBuff.cpp
@@ -108,10 +108,10 @@ void AMyXR_CharacterDeffenceBuff::OnBoardCalledFunctionServer(bool isOnBoard, bo
 
     if (isOnBoard)
     {
-        GetWorld()->GetTimerManager().SetTimer(LifeTimeTimerHandle, this, &AMyXR_CharacterDeffenceBuff::LifeTimeTimerFunction, 2.0f, false);
+        GetWorld()->GetTimerManager().SetTimer(LifeTimeTimerHandle, this, &AMyXR_CharacterDeffenceBuff::LifeTimeTimerFunction, 0.5f, false);
         if (CharacterType == ECharacterType::ECT_DefenceH)
         {
-            GetWorld()->GetTimerManager().SetTimer(HealTimerHandle, this, &AMyXR_CharacterDeffenceBuff::OnHealTimerTick, 0.5f, true, 0.0f);
+            GetWorld()->GetTimerManager().SetTimer(HealTimerHandle, this, &AMyXR_CharacterDeffenceBuff::OnHealTimerTick, 0.1f, true, 0.0f);
         }
     }
 
